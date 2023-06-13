f6cf4938a45b9b902fa4b69b521b5f1c5329d0b9
khantadzeeren <miusern1@gmail.com>
2023-06-13 13:57:39 +0400

    mongodb base config

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import fs from "fs";
 import yaml from "js-yaml";
-import { getAllJobs } from "./controllers/AllJob-controller.js";
+import { getAllJobs } from "./controllers/jobs-controller.js";
 dotenv.config();
 
 connect();
@@ -25,18 +25,13 @@ const options = {
   customSiteTitle: "DevJobs - Swagger API",
 };
 
-app.use(
-  express.static(path.join(new URL("../public", import.meta.url).pathname))
-);
 
-app.use("/images", express.static("./public/logos"));
+app.use("/images", express.static("public/logos"));
 
 app.get("/jobs/:size/:page", getAllJobs);
 
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000.`);
-});
+app.listen(3000);
 
 export default app;
--- src/controllers/jobs-controller.js
+++ src/controllers/jobs-controller.js
@@ -1,11 +1,12 @@
-import Job from "../models/Job.js";
+import Job from "../models/job.js";
 
 export const getAllJobs = async (req, res) => {
   const { size, page } = req.params;
+ 
   const limit = parseInt(size);
-  const skip = (parseInt(page) - 1) * limit;
+   const skip = (parseInt(page) - 1) * limit;
 
   const data = await Job.find().limit(limit).skip(skip);
 
-  return res.status(200).json(data);
+  return res.status(300).json(data);
 };
